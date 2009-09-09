dd431f1cdd8b2d6c620860c3786eacd5ee964a45
= <jamal@soueidan.com>
2009-09-09 21:05:57 +0200

    Added js_navigation_helpers

---
+++ lib/js_navigation_helpers.rb
@@ -0,0 +1,32 @@
+class Navigation < BlockHelpers::Base
+  def initialize(options={})
+    @options = options
+  end
+      
+  def display(body)
+    content_tag(:ul, body, @options)
+  end
+
+  def link(text, options={})
+    content_tag(:li, link_to_unless_current(text, options))
+  end
+  
+  
+  def link_selected(text, options={})
+    content_tag(:li, link_to(text, options.merge(:class => "selected")))
+  end
+  
+  def link_unselected(text, options={})
+    content_tag(:li, link_to(text, options.merge(:class => "unselected")))
+  end
+  
+  def link_back(text = nil)
+    text = "Back" if text.nil?
+    content_tag(:li, link_to(text, :back))
+  end
+  
+  alias :l :link
+  alias :ls :link_selected
+  alias :lus :link_unselected
+  alias :lb :link_back
+end
