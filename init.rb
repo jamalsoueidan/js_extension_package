1eb6fc7559428a4d8ce842765990f32581b9a399
= <jamal@soueidan.com>
2009-09-10 12:46:22 +0200

    Added post? and get? to controller

--- init.rb
+++ init.rb
@@ -2,3 +2,4 @@
 require 'block_helpers'
 require 'js_navigation_helpers'
 require 'js_custom_helpers'
+require 'js_extension_package'
--- lib/js_extension_package.rb
+++ lib/js_extension_package.rb
@@ -1,1 +1,12 @@
 # JsExtensionPackage
+module ExtendControllers
+  def post?
+    request.post?
+  end
+  
+  def get?
+    requrest.get?
+  end
+end
+
+ActionController::Base.send(:include, ExtendControllers)
