769f4d97e25d424c2777dcb362ceca9f7ca03a3b
= <jamal@soueidan.com>
2009-09-10 09:34:09 +0200

    Add new helper link_popup

--- lib/js_custom_helpers.rb
+++ lib/js_custom_helpers.rb
@@ -24,6 +24,14 @@ module CustomHelpers
     
     link_to(text, :back)
   end
+  
+  def link_popup(text, options = {})
+    if options.nil?
+      options = {}
+    end
+    
+    link_to(text, options, :popup => ['new_window_name', 'height=300,width=600,toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes'])
+  end
 end
 
 ActionView::Base.send(:include, CustomHelpers)
