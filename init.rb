e45e1e14b4cc24ff3e0ae575ee938ebca66dd29d
= <jamal@soueidan.com>
2009-09-10 21:33:15 +0200

    Reveret

--- init.rb
+++ init.rb
@@ -6,12 +6,3 @@ require 'js_flash_extensions'
 
 # block_helpers plugin
 require 'block_helpers'
-
-# validation_reflection plugin
-require 'boiler_plate/validation_reflection'
-
-ActiveRecord::Base.class_eval do
-  include BoilerPlate::ActiveRecordExtensions::ValidationReflection
-  BoilerPlate::ActiveRecordExtensions::ValidationReflection.load_config
-  BoilerPlate::ActiveRecordExtensions::ValidationReflection.install(self)
-end
