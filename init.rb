15d0665bdff6a7a167ec8595dc892f0cdcc1a3c4
= <jamal@soueidan.com>
2009-09-10 21:30:46 +0200

    Added boiler_plate plugin

--- init.rb
+++ init.rb
@@ -1,6 +1,17 @@
 # Include hook code here
-require 'block_helpers'
 require 'js_navigation_helpers'
 require 'js_custom_helpers'
 require 'js_extension_package'
 require 'js_flash_extensions'
+
+# block_helpers plugin
+require 'block_helpers'
+
+# validation_reflection plugin
+require 'boiler_plate/validation_reflection'
+
+ActiveRecord::Base.class_eval do
+  include BoilerPlate::ActiveRecordExtensions::ValidationReflection
+  BoilerPlate::ActiveRecordExtensions::ValidationReflection.load_config
+  BoilerPlate::ActiveRecordExtensions::ValidationReflection.install(self)
+end
