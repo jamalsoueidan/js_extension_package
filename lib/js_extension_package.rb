2e0758382931c9d1617bb6050f7c87a3622e94be
= <jamal@soueidan.com>
2009-09-10 14:45:01 +0200

    added et st nt for flash method

--- lib/js_extension_package.rb
+++ lib/js_extension_package.rb
@@ -7,6 +7,26 @@ module ExtendControllers
   def get?
     requrest.get?
   end
+  
+  # as flash[:error] = text
+  def et(text)
+    flash_translate(:error, text)
+  end
+
+  # as flash[:success] = text  
+  def st(text)
+    flash_translate(:success, text)
+  end
+  
+  # as flash[:notice] = text
+  def nt(text)
+    flash_translate(:notice, text)
+  end
+  
+  private
+    def flash_translate(key, text)
+      flash[key] = t("actioncontroller." + text)
+    end
 end
 
 ActionController::Base.send(:include, ExtendControllers)
