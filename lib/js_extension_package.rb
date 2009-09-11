81c99ae9a0801e94da69026da21b25b166aab16b
= <jamal@soueidan.com>
2009-09-11 11:09:04 +0200

    Added tt option to controllers

--- lib/js_extension_package.rb
+++ lib/js_extension_package.rb
@@ -8,6 +8,9 @@ module ExtendControllers
     requrest.get?
   end
 
+  def tt(text, options={})
+    t("actioncontroller." + text, options)
+  end
 end
 
 ActionController::Base.send(:include, ExtendControllers)
